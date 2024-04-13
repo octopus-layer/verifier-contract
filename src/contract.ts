8647c050d7e8752c7f4236be78a5b4e958a754d8
Yunus Gürlek <yunusguerlek@gmail.com>
2024-04-13 21:10:41 +0200

    Update contract logic

--- src/contract.ts
+++ src/contract.ts
@@ -1,57 +1,73 @@
-// Find all our documentation at https://docs.near.org
-import { NearBindgen, near, call, view, LookupMap, LookupSet, initialize } from 'near-sdk-js'
-import { assert } from './utils'
+import {
+  NearBindgen,
+  call,
+  view,
+  LookupSet,
+  UnorderedMap
+} from 'near-sdk-js';
+
 
 @NearBindgen({})
 class VerifierContract {
-  zkp_signature: string = "";
-  nodes =  new LookupSet<string>('');
+  node_keys = new LookupSet<string>('');
+  node_urls = new UnorderedMap<string>('');
   node_count: number = 0;
-  //zkp_list =  new LookupMap<string>( "zkp_list");
-  zkp_list = LookupMap<string>;
-
-  @call({}) 
-  deployContract(zkp_signature: string, nodes): void {
-    this.zkp_signature = zkp_signature;
-    this.nodes = nodes;
-  }
 
   @call({})
-  hashFunction(data: string): Uint8Array{
-    const dataBytes =  new TextEncoder().encode(data);
-    return near.sha256(dataBytes);
-  }
+  deployContract(
+    node_keys: string[], // Initial set of node public keys
+    node_urls: string[] // Initial set of node URLs
+  ): void {
+    if (node_keys.length != node_urls.length)
+      throw Error('Error: Node keys and URLs do not match.');
+
+    const urls = [];
+    
+    for (let i = 0; i < node_keys.length; i++)
+        urls[node_keys[i]] = node_urls[i];
+
+    this.node_keys.extend(node_keys);
+    this.node_urls.extend(urls);
+    this.node_count = node_keys.length;
+  };
 
   @call({})
-  addNode(newpubkey: string, signatures: [{
-    pubkey: string,
+  updateURL(
+    public_key: string,
+    new_url: string,
     signature: string
-  }]): void{
-     if (signatures.length < 0.66 * this.node_count){
-        assert(false, "Not enough signatures")
-     }
+  ): void {
+    if (!this.node_keys.contains(public_key))
+      throw Error('Error: Node does not exist.');
 
-     for (const { pubkey, signature } of signatures){
-        if (this.nodes.contains(pubkey)){
-          assert(false, "Node already exists")
-        }
-        
-     }
+    // TO DO: signature verification
 
-  }
+    this.node_urls.set(public_key, new_url);
+  };
 
   @call({})
-  settle_proof(proof: string, signatures: [{ pubkey: string, signature: string }]): void{
-      if (signatures.length < 0.66 * this.node_count){
-        assert(false, "Not enough signatures")
-      }
-      for (const { pubkey, signature } of signatures){
-        if (!this.nodes.contains(pubkey)){
-          assert(false, "Node does not exist")
-        }
-     
-      }
+  addNode(
+    public_key: string,
+    url: string,
+    signatures: [{
+      public_key: string;
+      signature: string;
+    }]
+  ): void {
+    if (this.node_keys.contains(public_key))
+      throw Error('Error: Node already exists.');
+
+    if (signatures.length < 0.66 * this.node_count)
+      throw Error('Error: Not enough signatures.');
+
+    for (const { public_key, signature } of signatures) {
+      if (!this.node_keys.contains(public_key))
+        throw Error('Error: Signature is not valid.');
+
+      // TO DO: signature verification
     }
 
+    this.node_keys.set(public_key);
+    this.node_urls.set(public_key, url);
   }
 }
